1745e4483b6bc31b5c0f21782d1ace29547db159
Zhuo Liu <vin_lz@outlook.com>
2019-12-08 22:42:16 -0500

    feat: Join a block

--- location.py
+++ location.py
@@ -23,6 +23,13 @@ def blocks():
     my_membership = get_my_membership()
     if my_membership:
         my_block_id = my_membership.bid
+        bpopulation = Blocks.query.filter_by(id=my_block_id).first().bpopulation
+        if my_membership.approval_count >= 3:
+            status = 'Joined'
+        elif my_membership.approval_count == bpopulation:
+            status = 'Joined'
+        else:
+            status = 'Pending'
     else:
         my_block_id = None
 
@@ -41,7 +48,7 @@ def blocks():
         my_blocks_hood_id = [my_block_id, my_hood_id]
         my_blocks_hood = [Blocks.query.filter_by(id=my_blocks_hood_id[0]).first(), Hood.query.filter_by(id=my_blocks_hood_id[1]).first()]
 
-        return render_template('blocks.html', my_blocks_hood=my_blocks_hood, blocks_hood=blocks_hood_list)
+        return render_template('blocks.html', status=status, my_blocks_hood=my_blocks_hood, blocks_hood=blocks_hood_list)
 
     print(blocks_hood_list)
     return render_template('blocks.html', blocks_hood=blocks_hood_list)
@@ -50,8 +57,13 @@ def blocks():
 @location.route('/blocks', methods=['POST'])
 @login_required
 def join_block():
-#     location = Location.query.filter_by(bid=block.id, cid=current_user.cid)
-#     hood = Hood.query.filter_by(id=Location.query.filter_by(block.))
+    to_join_block_id = request.form.get('to-join')
+    db.session.delete(get_my_membership())
+    db.session.commit()
+    db.session.add(Membership(uid=current_user.id, bid=to_join_block_id, approval_count=0))
+    db.session.commit()
+    flash('You are joining %s. Membership is pending approval' % Blocks.query.filter_by(id=to_join_block_id).first().bname)
+    
     return redirect(url_for('location.blocks'))
 
 def get_my_membership():
